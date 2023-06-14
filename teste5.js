5f216e3585475040ce5c8a39eaebadd3fd1f2367
bruna <brunaalmeida075@gmail.com>
2023-06-14 15:32:51 -0300

    testes feitos

---
+++ teste5.js
@@ -0,0 +1,14 @@
+const data = require('./fakeData');
+
+const readUser = (req, res) => {
+    const { name } =  req.query;
+    let count = 0;
+    data.filter((log) => {
+        if(log.name === name){
+            count += 1
+        }
+    })
+    res.send(`Usuário ${name} foi lido ${count} vezes.`);
+
+};
+module.exports = readUser;
